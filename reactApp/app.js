9e524d1f95d01e1405715a5996060001784edac6
Cindy <cindylay524@gmail.com>
2017-07-19 21:51:00 -0700

    yao this is still static

--- reactApp/app.js
+++ reactApp/app.js
@@ -4,12 +4,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-var dummyData = ["clean room", "call mom", "text my sister", "sleep 8 hours"]
+
+var dummyData = [{ taskText: "clean room", completed: false }, { taskText: "call mom", completed: false }, { taskText: "text Linda", completed: false }, { taskText: "sleep 8 hours", completed: false }]
 
 class Todo extends React.Component{
+  constructor(props){
+    super(props)
+  }
   render() {
     return (
-      <li><button> X </button> {this.props.task}</li>
+      <li>
+        <button onClick="myFunction()">
+          X
+        </button>
+          {this.props.completed ? <strike>{this.props.task}</strike> : this.props.task}
+      </li>
+
     )
   }
 }
@@ -19,12 +29,47 @@ class TodoList extends React.Component{
     return (
       <div>
         <ul>
-          {dummyData.map((item) => <Todo task ={item}/>)}
+          {this.props.todos.map((item) => <Todo key={item.taskText + Date.now()} completed={item.completed} task ={item.taskText}/>)}
         </ul>
       </div>
     );
   }
 }
 
-ReactDOM.render(<TodoList /> ,
+class InputLine extends React.Component{
+  render() {
+    return (
+      <div>
+          <input type="text" placeholder="Type Todo Here..." />
+          <input type="submit" value="Add Todo"/>
+      </div>
+    )
+  }
+}
+
+class TodoApp extends React.Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      todos: []
+    }
+  }
+
+  componentDidMount(){
+    this.setState({
+      todos: dummyData
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <InputLine />
+        <TodoList todos={this.state.todos} />
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<TodoApp /> ,
    document.getElementById('root'));
