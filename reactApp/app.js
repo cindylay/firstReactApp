70c258d5736518e151db05d6dfbb345cab3400e4
Cindy <cindylay524@gmail.com>
2017-07-19 17:56:56 -0700

    BASIC REACT APP

---
+++ reactApp/app.js
@@ -0,0 +1,35 @@
+// alert('app.js ran!');
+// console.log("it ran")
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// var dummyData = ["clean room", "call mom", "text my sister", "sleep 8 hours"]
+//
+// class Todo extends React.Component{
+//   render: function() {
+//     return (
+//       <div>
+//         <ul>
+//           {this.dummyData.map(dummyData => <li>{dummyData}</li>)}
+//         </ul>
+//       </div>
+//     );
+//   }
+// }
+//
+// class TodoList extends React.Component{
+//   render: function() {
+//     return (
+//       <div>
+//         <ul>
+//           {this.dummyData.map(dummyData => <li>{dummyData}</li>)}
+//         </ul>
+//       </div>
+//     );
+//   }
+// }
+
+ReactDOM.render(<p>Testing React</p>,
+   document.getElementById('root'));
+{/* <TodoList /> */}
